c02a648df0caf48add92843c32b8550e0af3bb1e
JohnnyHopSeedz <39112392+w00tcoin@users.noreply.github.com>
2023-07-23 16:08:21 -0700

    Add files via upload

---
+++ mails/de/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Prestashop Addons | Module by: <App1Pro>
+*
+* @author    Chuyen Nguyen [App1Pro].
+* @copyright Chuyenim@gmail.com
+* @license   http://app1pro.com/license.txt
+*/
+
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+header("Location: ../../");
+exit;
